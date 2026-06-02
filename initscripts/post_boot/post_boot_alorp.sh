8e5e00847147b854a1f15166ed52675905ec256e
rajulup <rajulu@qti.qualcomm.com>
2026-06-02 14:07:23 +0530

    urm: modified configs for multimedi usecases (#55)

--- initscripts/post_boot/post_boot_alorp.sh
+++ initscripts/post_boot/post_boot_alorp.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2026-2027, Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
-echo 128 > /proc/sys/kernel/sched_util_clamp_min_rt_default
+echo 0 > /proc/sys/kernel/sched_util_clamp_min_rt_default
 
 # Disable periodic kcompactd wakeups. We do not use THP, so having many
 # huge pages is not as necessary.
