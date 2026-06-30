0fbba9656e4be76b561829716a60280c91a69112
rajulup <rajulu@qti.qualcomm.com>
2026-06-30 20:32:47 +0530

    new target support for cq2390m (#87)
    Signed-off-by: rajulu <rajulu@qti.qualcomm.com>

---
+++ initscripts/post_boot/post_boot_cq2390m.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Copyright (c) 2026-2027, Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+echo 0 > /proc/sys/kernel/sched_util_clamp_min_rt_default
+
+# Disable periodic kcompactd wakeups. We do not use THP, so having many
+# huge pages is not as necessary.
+echo 0 > /proc/sys/vm/compaction_proactiveness
